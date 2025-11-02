8b1a347e3ca2c802035a4f7ea955a477646fd3ef
BKK <2715673327@QQ.COM>
2025-11-02 17:38:36 +0800

    feat: 添加搜索自动完成功能到组合框设置卡
    - 在 `EditComboBoxSettingCard` 类中新增 `_setupCompleter` 方法，启用组合框的搜索自动完成功能。
    - 更新 `addItems` 和 `setItems` 方法，以在添加选项后重新配置自动完成器。
    - 通过设置不区分大小写的匹配模式，提升用户体验。
    
    这些改动增强了组合框的交互性，使用户能够更方便地搜索和选择选项。

--- app/components/EditComboBoxSettingCard.py
+++ app/components/EditComboBoxSettingCard.py
@@ -2,6 +2,7 @@ from typing import List, Optional, Union
 
 from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QCompleter
 from qfluentwidgets import EditableComboBox, SettingCard
 from qfluentwidgets.common.config import ConfigItem, qconfig
 
@@ -30,6 +31,9 @@ class EditComboBoxSettingCard(SettingCard):
         for item in self.items:
             self.comboBox.addItem(item)
 
+        # 设置搜索功能
+        self._setupCompleter()
+
         # 设置布局
         self.hBoxLayout.addWidget(self.comboBox, 1, Qt.AlignRight)  # type: ignore
         self.hBoxLayout.addSpacing(16)
@@ -43,6 +47,16 @@ class EditComboBoxSettingCard(SettingCard):
         # 连接信号
         self.comboBox.currentTextChanged.connect(self.__onTextChanged)
         configItem.valueChanged.connect(self.setValue)
+
+    def _setupCompleter(self):
+        """设置搜索自动完成功能"""
+        if not self.items:
+            return
+
+        completer = QCompleter(self.items, self)
+        completer.setCaseSensitivity(Qt.CaseInsensitive)  # type: ignore # 不区分大小写
+        completer.setFilterMode(Qt.MatchContains)  # type: ignore # 包含匹配
+        self.comboBox.setCompleter(completer)
 
     def __onTextChanged(self, text: str):
         """当文本改变时触发"""
@@ -58,6 +72,8 @@ class EditComboBoxSettingCard(SettingCard):
         """添加选项"""
         for item in items:
             self.comboBox.addItem(item)
+        self.items.extend(items)
+        self._setupCompleter()
 
     def setItems(self, items: List[str]):
         """重新设置选项列表"""
@@ -65,3 +81,4 @@ class EditComboBoxSettingCard(SettingCard):
         self.items = items
         for item in items:
             self.comboBox.addItem(item)
+        self._setupCompleter()
